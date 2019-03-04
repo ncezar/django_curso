bbcb075a00690b3d5365cf9111b5e2e75390ad3f
Nathalia <nathalia.lcz@hotmail.com>
2019-03-04 08:25:41 -0300

    static tentativa 2 heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn mysite.wsgi --log-file -
-web: python curso/manage.py collectstatic --noinput; bin/gunicorn_django --workers=4 --bind=0.0.0.0:$PORT curso/settings.py 
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -119,5 +119,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+#STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
